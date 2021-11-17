b4e204fee3709fb1a626fd40dc359f00d4c0334b
Pavel.A.Ivanov <Pavel.A.Ivanov@wifire.ru>
2021-11-17 11:42:09 +0300

    + Footer

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,17 +2,19 @@ import React from 'react'
 import Header from "../components/Header/Header";
 import Nav from "../components/Nav/Nav";
 import Tariffs from "../components/Tariffs/Tariffs";
+import Footer from "../components/Footer/Footer";
 
 
 export default function App() {
    return (
       <>
-         <Nav />
-         <Header />
+         <Nav/>
+         <Header/>
          <main>
-            <Tariffs />
+            <Tariffs/>
 
          </main>
+         <Footer/>
       </>
    )
 }
