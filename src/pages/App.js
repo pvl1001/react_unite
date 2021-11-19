4e109356ac997a598da61811b002f8407702ca0f
Pavel.A.Ivanov <Pavel.A.Ivanov@wifire.ru>
2021-11-19 12:43:43 +0300

    + Support

--- src/pages/App.js
+++ src/pages/App.js
@@ -6,11 +6,13 @@ import Footer from "../components/Footer/Footer";
 import FAQ from "../components/FAQ/FAQ";
 import AppBanner from "../components/AppBanner/AppBanner";
 import WhatElse from "../components/WhatElse/WhatElse";
+import Support from "../components/Support/Support";
 
 
 export default function App() {
    return (
       <>
+         <Support/>
          <Nav/>
          <Header/>
          <main>
