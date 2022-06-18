bc225d385574f6832a6001c44d3b0532eed34493
pavel.and.ivanov <pavel.and.ivanov@megafon.ru>
2022-06-18 18:05:25 +0300

    upd ModalTariff sale banner

--- client/components/_modals/Tariff/Footer/Footer.js
+++ client/components/_modals/Tariff/Footer/Footer.js
@@ -3,6 +3,7 @@ import { showModal } from "../../../../redux/slices/modalsSlice";
 import { setDataOrder } from "../../../../redux/slices/orderSlice";
 import s from './Footer_new.module.sass';
 import { wrapp } from '../ModalTariff.module.sass';
+import SaleBanner from "../../../SaleBanner/SaleBanner";
 
 
 function Footer( { tariff, id } ) {
@@ -28,7 +29,9 @@ function Footer( { tariff, id } ) {
          <div className={ `${ s.container } ${ wrapp }` }>
             <div className={ s.price }>
 
-               <div className={ s.title }>{ tariff.name }</div>
+               <div className={ s.title }>{ tariff.name }
+                  <SaleBanner className={ s.sale_banner }/>
+               </div>
 
                <span className={ s.price__current }>{ tariff.totalPrice || tariff.price } ₽</span>
 
@@ -38,7 +41,6 @@ function Footer( { tariff, id } ) {
                   { Object.values( tariff.equipments ).some( eq => eq.switch ) &&
                      <span className={ s.price__desc }> с учетом выбранных опций</span>
                   }
-
                </nobr>
             </div>
 
