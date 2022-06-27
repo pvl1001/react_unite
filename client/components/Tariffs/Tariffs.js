df7100bbb96d98cf47251e8f80700633804c3f21
pavel.and.ivanov <pavel.and.ivanov@megafon.ru>
2022-06-27 10:34:16 +0300

    upd show modal cities

--- client/components/Tariffs/Tariffs.js
+++ client/components/Tariffs/Tariffs.js
@@ -3,15 +3,23 @@ import { swiperConfig } from "../../plugins_config";
 import React from "react";
 import { Swiper } from "swiper/react";
 import Tabs from "../Tabs/Tabs";
+import { useDispatch } from "react-redux";
+import { showModal } from "../../redux/slices/modalsSlice";
 
 
 function Tariffs( { children, tariffFilter } ) {
+   const dispatch = useDispatch()
+
+   function showModalCity() {
+      dispatch( showModal( { modal: 'cities', bool: true } ) )
+   }
+
 
    return (
       <section className={ 'tariffs ' + s.container }>
          <div className="wrapper">
 
-            <h1 className={ s.title }>Тарифы для твоего города: <span>Москва</span></h1>
+            <h1 className={ s.title }>Тарифы для твоего города: <span onClick={ showModalCity }>Москва</span></h1>
 
             <Tabs tariffFilter={ tariffFilter }/>
 
